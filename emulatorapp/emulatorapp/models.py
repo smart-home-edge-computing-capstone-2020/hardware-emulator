616793ad2600b494877c576164e0d656a58c89c6
Rip Lyster <rlyster@andrew.cmu.edu>
2020-04-05 19:17:39 -0400

    added models

--- emulatorapp/emulatorapp/models.py
+++ emulatorapp/emulatorapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Device(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+class Function(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    FUNCTION_TYPES = (
+        ('S', 'Sensor'),
+        ('A', 'Actuator')
+    )
+    function_type = models.CharField(max_length=1, choices=FUNCTION_TYPES)
