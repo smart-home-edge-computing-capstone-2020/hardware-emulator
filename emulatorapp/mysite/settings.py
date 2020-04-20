46035407fb4ef7b539285a2e26f979b7d5785dbe
Rip Lyster <rlyster@andrew.cmu.edu>
2020-04-19 23:34:37 -0400

    the ip change

--- emulatorapp/mysite/settings.py
+++ emulatorapp/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lq58_3!#pv6q(4c3r0s1b6h@*p2cal7*^4&wwncldrtnga2oms'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["172.31.35.122"]
+ALLOWED_HOSTS = ["18.224.96.41"]
 
 
 # Application definition
