3e4401896b5a7c949bb59e7521c7267b65254bb5
Rip Lyster <rlyster@andrew.cmu.edu>
2020-04-19 22:02:04 -0400

    removed unnecessary comments

--- hardware_library/HardwareLibrary.py
+++ hardware_library/HardwareLibrary.py
@@ -1,18 +1,5 @@
 import requests, json, copy
 
-# URL = "http://127.0.0.1:8000/emulator/"
-
-# if argv[1] == 'GET':
-#     r = requests.get(url=URL+sys.argv[2])
-#     data = r.json();
-#     print(data);
-
-# elif sys.argv[1] == 'POST':
-#     data = {'name':sys.argv[3],
-#             'description':sys.argv[4]}
-#     r = requests.post(url=URL+sys.argv[2], data=data)
-#     print(r.text)
-
 class HardwareLibrary:
     """
     Class initializer
